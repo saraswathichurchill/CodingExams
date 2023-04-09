3d0cb3a3fdcebc0d4a619b9cb170fac48b599332
ChurchillSaraswathi <45057335+saraswathichurchill@users.noreply.github.com>
2023-04-09 21:22:24 +0800

    Add files via upload
    Tax Brackets Assignment
    Try to imagine a world without taxes...
    But in the real world, we pay taxes, which means that someone calculates how much money we need to pay for taxes.
    
    So, I would ask you to write a program that takes the salary value as input and returns the tax due value as output.
    
    There are several tax brackets, and this means that different salaries will pay different taxes.
    
    The tax categories are determined as follows:
    from 0 to 3000 => 0%
    from 3000 to 5000 => 10%
    from 5000 to 7000 => 20%
    from 7000 to 9000 => 30%
    and from 9000 and above 40%
    
    For each bracket you should pay to relative amount for that specific bracket.
    Calculation examples:
    On a salary of 3,500 => 3000 * 0 + 500 * 0.1 = 50
    On a salary of 7,500 =>3000 * 0 + 2000 * 0.1 + 2000 *0.2 + 500 * 0.3 = 750
    On a salary of 10,000 => 3000 * 0 + 2000 * 0.1 + 2000 * 0.2 + 2000 * 0.3 + 100 * 0.4 = 1240
    
    The tax bracket definition can change over time so please write the code so it will be easy to change them.

---
+++ TaxCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator
+{
+    public class TaxBracket
+    {
+        public int RateId { get; set; }
+        public decimal MinRange { get; set; }
+        public decimal MaxRange { get; set; }
+        public decimal Rate { get; set; }
+        public decimal RateAmount { get; set; }
+    }
+    class Program
+    {
+        static List<TaxBracket> taxSlab = new List<TaxBracket>();
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Salary Amount: ");
+           
+            decimal salary = Convert.ToDecimal(Console.ReadLine().Trim());
+           
+            var taxToBePaid = 0m;
+            taxSlab.AddRange(new TaxBracket[]
+               {
+                    new TaxBracket{RateId=1,MinRange=0,MaxRange=3000, Rate=0m},
+                    new TaxBracket{RateId=2,MinRange=3000, MaxRange=5000,Rate=0.1m },
+                    new TaxBracket{RateId=3,MinRange=5000,MaxRange=7000,Rate=0.2m},
+                    new TaxBracket{RateId=4,MinRange=7000,MaxRange=9000,Rate=0.3m},
+                    new TaxBracket{RateId=5,MinRange=9000,MaxRange=decimal.MaxValue,Rate=0.4m}
+               }
+               );
+            foreach (var slab in taxSlab)
+            {
+                            
+                if(salary>slab.MinRange)
+                {
+                    var taxableRate = Math.Min(slab.MaxRange - slab.MinRange, salary - slab.MinRange);
+                    var taxThisSlab = taxableRate * slab.Rate;
+                    taxToBePaid = taxToBePaid + taxThisSlab;
+                }
+            }
+            Console.WriteLine("Your Salary is : {0}, and You need to Pay the Tax Amount as: {1}",salary,taxToBePaid);
+            Console.ReadKey();
+        }
+    }
+}
